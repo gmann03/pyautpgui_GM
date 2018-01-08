19f25b34dc1e01482b6914092a3028cffe37b679
gmann03 <33422422+gmann03@users.noreply.github.com>
2018-01-08 08:26:11 -0500

    Initial Upload

---
+++ PG_GM.py
@@ -0,0 +1,24 @@
+import pyautogui as pg
+import time
+pg.hotkey('winleft','ctrl','d')
+pg.hotkey('winleft')
+pg.typewrite('chrome\n',0.1)
+pg.hotkey('winleft', 'up')
+pg.typewrite('http://crouton.net/\n')
+time.sleep(5)
+pg.hotkey('ctrl', 't')
+pg.typewrite('http://www.staggeringbeauty.com/\n)')
+time.sleep(10)
+pg.hotkey('ctrl', 't')
+pg.typewrite('http://www.republiquedesmangues.fr/\n')
+time.sleep(5)
+pg.hotkey('ctrl', 't')
+pg.typewrite('http://corgiorgy.com/\n')
+time.sleep(10)
+pg.hotkey('ctrl', 't')
+pg.typewrite('http://www.ismycomputeron.com/\n')
+time.sleep(10)
+pg.hotkey('ctrl', 't')
+pg.typewrite('http://www.theuselessweb.com/\n')
+
+
